cecd013a2f7c069db24af7f26c5017643c63abff
Tumo M <tumomasire@gmail.com>
2020-01-12 01:26:38 +0200

    Basic Components and Styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,75 @@
 import React from 'react';
 import './App.css';
-import Navigation from './components/Navigation/Navigation'
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import tachyons from 'tachyons';
+import Particles from 'react-particles-js';
 
-function App() {
+const particleOptions = {
+  particles: {
+    "number": {
+      "value": 70,
+      "density": {
+        "enable": true,
+        "value_area": 500
+      }
+    },
+    "shape": {
+      "type": "polygon",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      }
+    },
+    
+      "move": {
+        "enable": true,
+        "speed": 8,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": true,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+      "interactivity": {
+        "detect_on": "window",
+        "events": {
+          "onhover": {
+            "enable": false,
+            "mode": "repulse"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "push"
+          },
+          "resize": true
+        }
+      }
+  }
+
+class App extends React.Component {
+  render(){
   return (
     <div className="App">
+      <Particles className="particles" 
+        params={particleOptions} 
+      />
       <Navigation />
-      {/*<Logo />
+      <Logo />
+      <Rank />
       <ImageLinkForm />
+      {/*
       <FaceRecognition />*/}
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Rank.css';
+
+
+const Rank = () =>{
+return(
+    <div className="ma4 mt0">
+       <div className="white f3">
+        {`Tumo, your current rank is...`}
+       </div>
+       <div className="white f1">
+        {`#2!!!`}
+       </div>
+    </div>
+)
+
+}
+
+export default Rank;
