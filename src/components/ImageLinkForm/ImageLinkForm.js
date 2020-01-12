9f0937c31c3c21f762786d17419c61f7e5f7fbeb
Tumo M <tumomasire@gmail.com>
2020-01-12 15:23:05 +0200

    Fixed broken picture submit button.

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ImageLinkForm.css';
 
 
-const ImageLinkForm = ({onInputChange, onSubmitBtn}) =>{
+const ImageLinkForm = ({onInputChange, onPictureSubmit}) =>{
 return(
     <div className="ma4 mt0">
         <p className="f3">
@@ -11,7 +11,7 @@ return(
         <div className="center grow">
         <div className='form center pa4 br3 shadow-5'>
             <input onChange={onInputChange} type="text" className="f4 pa2 w-70 center"/>
-            <button onClick={onSubmitBtn} className="w-30 grow link ph3 pv2 dib white bg-light-purple">Detect</button>
+            <button onClick={onPictureSubmit} className="w-30 grow link ph3 pv2 dib white bg-light-purple">Detect</button>
         </div>
         </div>
     </div>
